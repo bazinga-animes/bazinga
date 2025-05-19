45d28b9b92c25b63d4383bb3098645765203ef17
bazinga-animes <bazingaanimesapp@gmail.com>
2025-05-19 14:51:49 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const app = express();
+
 app.get("/", (req, res) => {
   res.send("🚀 SugoiAPI está rodando!");
+});
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log(`SugoiAPI rodando na porta ${PORT}`);
 });
