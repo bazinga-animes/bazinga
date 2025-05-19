7705ccd0cbc62a7e80634f53b8fc3722eb95cb2c
bazinga-animes <bazingaanimesapp@gmail.com>
2025-05-19 13:56:27 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Sugoi = require("./src/Scraper/scraper");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1000;
 
 app.use(cors());
 
